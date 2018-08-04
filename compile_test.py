344e6b0c839db5aac8ff6955a97bc3061c864bd2
Langwen Huang <huanglangwen10@gmail.com>
2018-08-04 13:47:38 +0100

    Change project structure, add IDEA

--- compile_test.py
+++ compile_test.py
@@ -5,6 +5,8 @@ if not ("CLASSPATH" in os.environ):
 os.environ["CLASSPATH"]=os.path.abspath("./antlr/antlr-4.7.1-complete.jar")+";"+os.environ["CLASSPATH"]
 files=["MCMLexer.g4","MCMParser.g4"]
 for f in files:
-    os.system(f"java org.antlr.v4.Tool -Dlanguage=Python3 {f}")
-os.system(f"python ./antlr/pygrun MCM reactions --tokens ./mechanism_files/MCM_APINENE.eqn.txt > tokens.txt")
-os.system(f"python ./antlr/pygrun MCM reactions --tree ./mechanism_files/MCM_APINENE.eqn.txt > ast.txt")
+    os.system(f"java org.antlr.v4.Tool -Dlanguage=Python3 -o gen {f}")
+os.chdir("gen")
+os.system(f"python ../antlr/pygrun MCM reactions --tokens ../mechanism_files/MCM_APINENE.eqn.txt > tokens.txt")
+os.system(f"python ../antlr/pygrun MCM reactions --tree ../mechanism_files/MCM_APINENE.eqn.txt > ast.txt")
+os.chdir("..")
